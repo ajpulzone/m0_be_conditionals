f2ed7f86a9922ff5cbbbcb56eecff3294073170b
Annie Pulzone <ajpulzone@gmail.com>
2022-07-11 12:45:55 -0700

    Add spicy challenge

---
+++ code_challenges/spicy.rb
@@ -0,0 +1,12 @@
+#Spicy
+
+good_driving_record = true
+is_over_25 = true
+
+if good_driving_record == true and is_over_25 == true
+  puts "You will be getting a discount on your car rental. Thank you for being a responsible driver"
+elsif good_driving_record != true and is_over_25 != true
+  puts "We can't rent you a car without someone else signing the rental agreement."
+elsif good_driving_record != true or is_over_25 != true
+  puts "You will be paying full price on your car rental"
+end
